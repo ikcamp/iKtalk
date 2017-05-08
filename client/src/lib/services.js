461643bb0e886c2977d85e8645174de99aa90f65
dailiang <dailiang@hujiang.com>
2017-05-08 18:00:38 +0800

    update: 更新客户端，连接socket

--- client/src/lib/services.js
+++ client/src/lib/services.js
@@ -1,4 +1,6 @@
 import MediaStreamRecorder from 'msr';
+import io from 'socket.io-client'
+import ss from 'socket.io-stream'
 const mediaConstraints = {
   audio: true,
   video: {
@@ -10,6 +12,14 @@ const mediaConstraints = {
   }
 }
 let streamCb = null;
+
+let socket = io('https://192.168.37.118:5801/test')
+
+socket.on('connect', () => {
+  socket.emit('login', {
+    id: '705730534'
+  })
+})
 
 function getLocalStream(isFront, callback) {
   streamCb = callback;
@@ -23,15 +33,18 @@ function onMediaSuccess(stream) {
   var mediaRecorder = new MediaStreamRecorder(stream);
   mediaRecorder.mimeType = 'video/webm';
   mediaRecorder.ondataavailable = function (blob) {
-      // POST/PUT "Blob" using FormData/XHR2
-      var blobURL = URL.createObjectURL(blob);
-      window.open(blobURL);
+    // POST/PUT "Blob" using FormData/XHR2
+    let sr = ss.createStream()
+    ss(socket).emit('upload', sr)
+    ss.createBlobReadStream(blob).pipe(sr)
+    // var blobURL = URL.createObjectURL(blob);
+    // window.open(blobURL);
   };
-  mediaRecorder.start(5000);
+  mediaRecorder.start(3000);
 }
 
 function onMediaError(e) {
-    console.error('media error', e);
+  console.error('media error', e);
 }
 
 export {
