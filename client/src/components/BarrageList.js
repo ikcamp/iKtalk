14ad1879e41870d8e9795ce79ab1069c805c2f01
luckyhalley <mky81@126.com>
2017-05-20 23:11:21 +0800

    Added:barrage components

---
+++ client/src/components/BarrageList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+export default class BarrageList extends Component {
+    constructor(props) {
+      super(props)
+    }
+    render() {
+      return (
+        <ul>
+          <li>TODO</li>
+        </ul>
+      )
+    }
+}
